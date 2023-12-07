c52234e13b7da8be3d80cb72f970c300e32ac2f3
ShiveyVer <marklin9133131@gmail.com>
2023-12-07 18:03:12 +0800

    task3~

--- README.md
+++ README.md
@@ -26,3 +26,23 @@
     data_resampled.csv (training)
     testing_data.csv (for prediction)
     ⇒ predict.py ⇒ predicted_results.csv
+## TEST3
+
+-    *fill the first col-name with id in both testing/training data*
+
+- **Preprocessing**
+    1. *replacing missing value with mean*
+        
+        training_data.csv ⇒ impute.py ⇒ data_filled_mean.csv
+        
+    2. *resample data with SMOTE*
+        
+        data_filled_mean.csv ⇒ resample.csv ⇒ data_resampled.csv
+        
+- **Prediction**
+    
+    *scaling then use tf.keras.models.Sequential for training/prediction*
+    
+    data_resampled.csv (training)
+    testing_data.csv (for prediction)
+    ⇒ predict.py ⇒ predicted_results.csv
---
+++ test3/impute.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# 讀取CSV文件
+file_path = 'testing_data.csv'
+data = pd.read_csv(file_path)
+
+# 保留 label 欄位
+# labels = data['label']
+# data = data.drop('label', axis=1)
+
+# 指定需要檢查的欄位
+columns_to_check = ['x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15', 'x16']
+
+# 創建一個新的 DataFrame 來保存不同填補方式的資料
+data_filled_mean = data.copy()
+
+# 使用平均值填補缺失值
+data_filled_mean[columns_to_check] = data_filled_mean[columns_to_check].apply(lambda x: x.fillna(x.mean()))
+
+# data_filled_mean['label'] = labels
+
+# 將結果保存為新的 CSV 文件
+output_file_path_mean = 'testing_data_filled_mean.csv'
+
+data_filled_mean.to_csv(output_file_path_mean, index=False)
+
+# 顯示成功訊息
+print(f"以平均值填補缺失值的資料已保存至 {output_file_path_mean}")
